3a074e94dbfb1f1c92130b0c1b57c697ee62ba9d
Jyoti <jg.jyotigaikwad121@gmail.com>
2026-09-04 14:23:44 +0530

    Before_DSA_Program_Practice

---
+++ Phase1_BeforeDSA.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace BeforeDSA_Program.Phase1;
+
+public class Phase1_BeforeDSA
+{
+    public static void Main(string[] args)
+    {
+        PrintNum1to10();
+    }
+    //1. Print all numbers from 1 to 10 using a loop.
+
+    public static void PrintNum1to10()
+    {
+        int i = 1;
+
+        while (i <= 10){
+            i++;
+            Console.WriteLine(i);
+        }
+
+
+    }
+}
